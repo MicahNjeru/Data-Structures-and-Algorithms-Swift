2d4c4f8cfae3816886d9e393b0c92095557ce904
Micah Njeru <micahnjeru1@gmail.com>
2023-09-09 16:13:29 +0300

    Added Maximum consecutive ones (1s) problem with solution algorithm

---
+++ Maximum-consecutive-ones-(1s).playground/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+/// Q1: Max Consecutive Ones
+/// Given a binary array, find the maximum number of consecutive 1s in this array. Example:
+/// Input: [1, 1, 0, 1, 1, 1]
+/// Output: 3
+/// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+func maxConsOne(_ arr: [Int]) -> Int {
+    var localCount = 0 // 1
+    var globalCount = 0 // 1
+    
+    for item in arr { // n
+        if item == 1 {
+            localCount += 1 //n
+            globalCount = max(localCount, globalCount) // n
+        } else {
+            localCount = 0 // n
+        }
+    }
+    
+    return globalCount // 1
+}
+
+print(maxConsOne([1, 1, 0, 1, 1, 1]))
+print(maxConsOne([1, 1, 0, 1, 1]))
+print(maxConsOne([0, 0, 0, 0, 0]))
+print(maxConsOne([1, 1, 1, 1, 1, 1]))
+print(maxConsOne([1, 1, 1, 1, 1, 1]))
+
+
+// 1 + 1 + n + n + n + 1 = 3 + 3n
+// Solution time complexity: 3n -> O(n)
