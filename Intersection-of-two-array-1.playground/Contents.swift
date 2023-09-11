e4dcfe3c15ae2c20cfce5679d4f6c08e5c43e2b7
Micah Njeru <micahnjeru1@gmail.com>
2023-09-11 16:29:56 +0300

    Added intersection of two arrays 1 playground

---
+++ Intersection-of-two-array-1.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/// Q1: Given two arrays, write a function to compute their intersection.
+/// Example:
+/// Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+/// Note:
+/// - Each element in the result must be unique.
+/// - The result can be in any order.
+/// Solution: Basic concept is to create two sets to store unique elements and then use intersection method of Hashset to extract unique common elements.
+
+
+
+
