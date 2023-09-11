9e584004c2a3afd3d4d14a9cfbee01096a73e368
Micah Njeru <micahnjeru1@gmail.com>
2023-09-11 16:39:03 +0300

    Added code solution and time complexity calculation

--- Intersection-of-two-array-1.playground/Contents.swift
+++ Intersection-of-two-array-1.playground/Contents.swift
@@ -8,6 +8,28 @@ import Foundation
 /// - The result can be in any order.
 /// Solution: Basic concept is to create two sets to store unique elements and then use intersection method of Hashset to extract unique common elements.
 
+func inter(_ arr1: [Int], _ arr2: [Int]) -> [Int] {
+    // We create an empty set to store unique values from first array
+    var set1 = Set<Int>() // 1
+    
+    // We loop through the 1st array and insert elements to the 1st set
+    for item in arr1 { //n
+        set1.insert(item)
+    }
+    
+    // We create an empty set to store unique values from second array
+    var set2 = Set<Int>() // 1
+    
+    // We loop through the 2nd array and insert elements to the 2nd set
+    for item in arr2 { // n
+        set2.insert(item)
+    }
+    
+    // We check for elements that intersect in both sets and return them as an array
+    return Array(set1.intersection(set2)) // n
+}
 
+print(inter([1, 2, 2, 1], [2, 2]))
 
-
+// 1 + n + 1 + n + n = 2 + 3n
+// Solution time complexity: 3n -> O(n)
