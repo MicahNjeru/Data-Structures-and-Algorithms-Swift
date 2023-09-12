894f301a4f73e0653cfd72d31e3a01eb2a734add
Micah Njeru <micahnjeru1@gmail.com>
2023-09-12 19:16:37 +0300

    Added code for contains-duplicate and created contains-duplicate-with-distance playground

--- Contains-duplicate.playground/Contents.swift
+++ Contains-duplicate.playground/Contents.swift
@@ -14,6 +14,32 @@ import Foundation
 /// Input: [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
 /// Output: true
 ///
-/// Solution:
+/// Solution: We use learnings the previous challenges, specifically the dictionary map technique.
+
+func containsDup(_ arr: [Int]) -> Bool {
+    // We create an empty dictionary
+    var dict = [Int: Int]() // 1
+    
+    // We loop over the array
+    for item in arr { // n
+        // checking if the item is already in the dictionary, ...
+        // if it is, we return true and exit the function otherwise ...
+        if dict[item] != nil {
+            return true
+            // we add it and assign a value of 1
+        } else {
+            dict[item] = 1
+        }
+    }
+    return false // 1
+}
 
 
+// Function test
+print(containsDup([1, 2, 3, 1]))
+print(containsDup([1, 2, 3, 4]))
+print(containsDup([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+
+
+// 1 + 1 + n = 2 + n
+// Solution time complexity: n -> O(n)
