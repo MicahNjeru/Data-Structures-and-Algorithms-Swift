892577101c9f5bc1eff057cabfeeb3fe1d919749
Micah Njeru <micahnjeru1@gmail.com>
2023-09-15 17:21:12 +0300

    Added FizzBuzz playground

---
+++ FizzBuzz.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+/// Q1: Write a program that outputs the string representation of numbers from 1 to n.
+/// But for multiples of three it should output "Fizz" instead of the number and for the multiples of five output "Buzz".
+/// For numbers which are multiples of both three and five output "FizzBuzz".
+/// Example: n = 15
+/// Output: [ "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
+/// Solution:
