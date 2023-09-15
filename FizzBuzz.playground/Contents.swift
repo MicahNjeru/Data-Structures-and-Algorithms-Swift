d84bee1a7595e76ca58e1fd685d2a6166a8fd217
Micah Njeru <micahnjeru1@gmail.com>
2023-09-15 18:05:34 +0300

    Added comments to FizzBuzz

--- FizzBuzz.playground/Contents.swift
+++ FizzBuzz.playground/Contents.swift
@@ -8,14 +8,23 @@ import Cocoa
 /// Solution: Main idea behind this is to handle multiples of 3 and multiples of 5 separately but handling multiples of both 3 and 5 before handling them individually if n is less then or equal to 0 then return
 
 func fizzBuzz(_ number: Int) {
+    // We loop over the number ...
     for i in 1...number { // n
+        // checking if it's divisible by 3 AND divisible by 5 first because
+        // it will first check if it's divisble by 3 and never check if it's also divisible by 5 or
+        // it will first check if it's divisble by 5 and never check if it's also divisible by 3
+        // depending with how it's ordered
         if i % 3 == 0 && i % 5 == 0 {
+            // If true, print FizzBuzz ...
             print("FizzBuzz")
         } else if i % 3 == 0 {
+            // else print Fizz if it's only divisible by 3
             print("Fizz")
         } else if i % 5 == 0 {
+            // else print Fizz if it's only divisible by 5
             print("Buzz")
         } else {
+            // print the number if it's not divisible by both 3 AND 5 OR 3 OR 5
             print(i)
         }
     }
--- Palindrome-string.playground/Contents.swift
+++ Palindrome-string.playground/Contents.swift
@@ -7,6 +7,31 @@ import Cocoa
 /// Explanation: reversing foo produces oof meaning it's not a palindrome
 /// Input: bob
 /// Output: True
+/// Solution: Create two pointers, one starting from the beginning of the string and another from the end and compare two chars at those indexes.
+/// If two pointers point at chars that are equal then increment startPointer and decrement endPointer till they both meet in the middle.
+/// If at any point two chars are not equal then you have non-palindromic string
 
 
+func isPalindrome(_ str: String) -> Bool {
+    var arr = Array(str.lowercased()) // 1
+    var i = 0 // 1
+    var j = arr.count - 1 // 1
+    
+    while i <= j { // n
+        if arr[i] == arr[j] {
+            i += 1
+            j -= 1
+        } else {
+            return false
+        }
+    }
+    return true // 1
+}
+
+// Function test
+isPalindrome("foo")
+isPalindrome("bob")
+
+// 1 + 1 + 1 + n + 1 = 4 + n
+// Time complexity solution: n -> O(n)
 
