bdc37c9417b67f07cc2ab6e17a1b8bedcaec8446
Micah Njeru <micahnjeru1@gmail.com>
2023-09-15 17:44:25 +0300

    Added palindromic string playground

---
+++ Palindrome-string.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+/// A palindromic string is a string which reads same when it is reversed. Write a func to check if string is palindrome or not
+/// Example:
+/// Input: foo
+/// Output: False
+/// Explanation: reversing foo produces oof meaning it's not a palindrome
+/// Input: bob
+/// Output: True
+
+
+
