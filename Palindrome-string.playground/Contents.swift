4e795e649c8f5ef8181f655dae3135dbe48ddf41
Micah Njeru <micahnjeru1@gmail.com>
2023-09-15 18:23:55 +0300

    Added code for Alphanumeric palindrom test, explainer comments and time complexity solution

--- Palindrome-string.playground/Contents.swift
+++ Palindrome-string.playground/Contents.swift
@@ -21,6 +21,18 @@ func isPalindrome(_ str: String) -> Bool {
     
     // We check if i and j have reached the middle yet, that is they are equal if so ...
     while i <= j { // n
+        // We check if the input being looped over at i is a letter or number ...
+        if isAlphaNum(arr[i]) == false && i < j {
+            // if it is, we increment i counter and continue with the while loop
+            i += 1
+            continue
+        }
+        // We check if the input being looped at j over is a letter or number ...
+        if isAlphaNum(arr[j]) == false && i < j {
+            // if it is, we decrement j counter and continue with the while loop
+            j -= 1
+            continue
+        }
         // we check if the common element is the same, if it is ...
         if arr[i] == arr[j] {
             // we increment i and decrement j
@@ -35,10 +47,21 @@ func isPalindrome(_ str: String) -> Bool {
     return true // 1
 }
 
-// Function test
+// Alpha-character Palindrome Function test
 isPalindrome("foo")
 isPalindrome("bob")
+isPalindrome("A man, a plan, a canal: Panama")
 
 // 1 + 1 + 1 + n + 1 = 4 + n
 // Time complexity solution: n -> O(n)
 
+
+/// We can also be asked to check if the input is a palindrom when it's an Alphaneumeric input
+
+func isAlphaNum(_ char: Character) -> Bool {
+    // We check if the input is either number OR letter and return the value
+    return char.isNumber || char.isLetter // 1
+}
+
+// = 1
+// Time complexity solution: 1 -> 1
