69b0ac2ab7a739d1472be814c07aa317d6acc72c
Micah Njeru <micahnjeru1@gmail.com>
2023-09-16 18:19:04 +0300

    Added valid-palindrome-2 playground

--- Valid-palindrome-2.playground/Contents.swift
+++ Valid-palindrome-2.playground/Contents.swift
@@ -1,3 +1,17 @@
 import Cocoa
 
-/// 
+/// Given a non-empty strings, you may delete at most one character. Judge whether you can make it a palindrome.
+/// Example:
+/// Input: aba
+/// Output: true
+/// Input: abca
+/// Output: true
+/// Solution: Keep a flag for first encountered out of order element and continue to loop forward
+
+
+
+// Function test
+
+
+// + + + + =
+// Time complexity solution: n -> O(n)
